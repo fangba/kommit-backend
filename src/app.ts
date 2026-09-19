8da175de37d3cfaa3973cc9d73bba436e6067198
fangba <franck.angba@yahoo.fr>
2026-09-19 23:29:29 +0000

    feat: whitelist des origins CORS autorisées
    Règle métier 8 du ticket KOM-03 : localhost:5173 à 5176.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
+import cors from 'cors'
 import express from 'express'
+import { corsOptions } from '@/config/corsOptions.js'
 import { errorHandler } from '@/middlewares/errorHandler.js'
 import { notFoundHandler } from '@/middlewares/notFoundHandler.js'
 import { routes } from '@/routes/index.js'
 
 export const app = express()
 
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(routes)
 app.use(notFoundHandler)
---
+++ src/config/corsOptions.ts
@@ -0,0 +1,12 @@
+import type { CorsOptions } from 'cors'
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'http://localhost:5175',
+  'http://localhost:5176',
+]
+
+export const corsOptions: CorsOptions = {
+  origin: allowedOrigins,
+}
